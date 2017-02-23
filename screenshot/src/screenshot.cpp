382c332b1f2da2bbd0f02c7bfd19aff5c5ecbd22
Björn Ritzl <bjorn.ritzl@king.com>
2017-02-23 09:18:22 +0100

    Cleanup

--- screenshot/src/screenshot.cpp
+++ screenshot/src/screenshot.cpp
@@ -15,15 +15,10 @@ static GLubyte* ReadPixels(unsigned int x, unsigned int y, unsigned int w, unsig
   return data;
 }
 
-static void DumpPixels(GLubyte* pixels, unsigned int from, unsigned int to) {
-  for (unsigned int i=from; i < to; i++) {
-    printf("%i R:%02x G:%02x B:%02x A:%02x\n", i, pixels[0 + (i * 4)], pixels[1 + (i * 4)], pixels[2 + (i * 4)], pixels[3 + (i * 4)]);
-  }
-}
-
 static int Pixels(lua_State* L) {
   int top = lua_gettop(L);
 
+  // read pixels
   const int x = luaL_checkint(L, 1);
   const int y = luaL_checkint(L, 2);
   const int w = luaL_checkint(L, 3);
@@ -83,32 +78,36 @@ static int Png(lua_State* L) {
 static int Buffer(lua_State* L) {
   int top = lua_gettop(L);
 
+  // read pixels
   const int x = luaL_checkint(L, 1);
   const int y = luaL_checkint(L, 2);
   const int w = luaL_checkint(L, 3);
   const int h = luaL_checkint(L, 4);
   GLubyte* pixels = ReadPixels(x, y, w, h);
 
+  // create buffer
   dmBuffer::HBuffer buffer;
   dmBuffer::StreamDeclaration streams_decl[] = {
     { dmHashString64("pixels"), dmBuffer::VALUE_TYPE_UINT8, 1 }
   };
   dmBuffer::Result r = dmBuffer::Allocate(w * h * 4, streams_decl, 1, &buffer);
   if (r == dmBuffer::RESULT_OK) {
+    // copy pixels into buffer
     uint8_t* data = 0;
     uint32_t datasize = 0;
     dmBuffer::GetBytes(buffer, (void**)&data, &datasize);
     memcpy(data, pixels, datasize);
     delete pixels;
 
+    // validate and return
     if (dmBuffer::ValidateBuffer(buffer) == dmBuffer::RESULT_OK) {
       dmScript::PushBuffer(L, buffer);
     }
     else {
       lua_pushnil(L);
     }
-    //dmBuffer:Free(buffer);
   }
+  // buffer creation failed
   else {
     delete pixels;
     lua_pushnil(L);
